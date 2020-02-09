be900dcdb6893e6d5242ca5c93ec61606146ad8c
Robin D'Andrea <robin.dandrea@versalitic.com>
2020-02-09 11:54:52 +0100

    parser angefangen

--- src/basic/Parser/Parser.cs
+++ src/basic/Parser/Parser.cs
@@ -1,22 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
 namespace LearnCsStuf.Basic
 {
-    public class Parser : IEnumerator, IEnumerable
+    public class Parser
     {
-
-        // -----------------------------------------------
-
-        #region vars
-
-        // -----------------------------------------------
-
-        private int position;
-
-        // -----------------------------------------------
-
-        #endregion vars
 
         // -----------------------------------------------
 
@@ -24,32 +12,31 @@ namespace LearnCsStuf.Basic
 
         // -----------------------------------------------
 
-        public string Text
+        public string File
         {
             get;
+            set;
         }
 
         // -----------------------------------------------
 
-        public List<ILexerToken> LexerTokens
+        public string ExpressionLine
         {
             get;
-        } = new List<ILexerToken> ();
-
-        // -----------------------------------------------
-
-        private char current
-        {
-            get
-            {
-                if (this.IsEnde (  )) return '\0';
-                return this.Text[position];
-            }
+            set;
         }
 
         // -----------------------------------------------
 
-        public object Current
+        private Lexer Tokenizer
+        {
+            get;
+            set;
+        }
+
+        // -----------------------------------------------
+
+        public List<SyntaxToken> SyntaxErrors
         {
             get;
             set;
@@ -61,97 +48,102 @@ namespace LearnCsStuf.Basic
 
         // -----------------------------------------------
 
-        #region ctor
-
-        // -----------------------------------------------
-
-        /*public Parser ( List<> )
-        {
-            this.Text = text;
-        }*/
-
-        // -----------------------------------------------
-
-        #endregion ctor
-
-        // -----------------------------------------------
-
         #region methods
 
         // -----------------------------------------------
 
-        public SyntaxToken NextToken (  )
+        private bool InitLexer (  )
         {
-            if (this.IsEnde (  ) ) return null;
+            this.Tokenizer = new Lexer ( this.ExpressionLine );
 
-            foreach ( ILexerToken lexerToken in this.LexerTokens )
+            this.Tokenizer.LexerTokens.Add ( new Operator ( '+', '-', '*', '/', '%', '&', '|', '=', '<', '>', '!', '^', '~' ) );
+            this.Tokenizer.LexerTokens.Add ( new Digit (  ) );
+            this.Tokenizer.LexerTokens.Add ( new Whitespaces (  ) );
+            this.Tokenizer.LexerTokens.Add ( new OpenKlammer (  ) );
+            this.Tokenizer.LexerTokens.Add ( new CloseKlammer (  ) );
+            this.Tokenizer.LexerTokens.Add ( new Text (  ) );
+            this.Tokenizer.LexerTokens.Add ( new GeschweifteKlammerAuf (  ) );
+            this.Tokenizer.LexerTokens.Add ( new GeschweifteKlammerZu (  ) );
+            this.Tokenizer.LexerTokens.Add ( new EckigeKlammerZu (  ) );
+            this.Tokenizer.LexerTokens.Add ( new EckigeKlammerAuf (  ) );
+            this.Tokenizer.LexerTokens.Add ( new Point (  ) );
+            this.Tokenizer.LexerTokens.Add ( new Comma (  ) );
+            this.Tokenizer.LexerTokens.Add ( new DoublePoint (  ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "int" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "char" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "byte" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "for" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "while" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "bool" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "true" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "null" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "enum" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "continue" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "break" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "false" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "void" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "return" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "class" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "static" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "nonref" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "public" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "private" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "uint" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "ushort" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "short" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "long" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "ulong" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "ref" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "is" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "in" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "as" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "foreach" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "float" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "new" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "delegate" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "using" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "this" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "base" ) );
+            this.Tokenizer.LexerTokens.Add ( new KeyWord ( "sizeof" ) );
+            this.Tokenizer.LexerTokens.Add ( new Words ( new List<ILexerToken> () { new HigherAlpabet (  ), new LowerAlpabet (  ), new Digit (  ), new Underscore (  ) } ) );
+            this.Tokenizer.LexerTokens.Add ( new Semikolon (  ) );
+
+            return true;
+        }
+
+        // -----------------------------------------------
+
+        public bool PrintSyntaxError(SyntaxToken token)
+        {
+            if (token.Kind != SyntaxKind.Unknown) return false;
+
+            this.SyntaxErrors.Add ( token );
+
+            ConsoleColor colr = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Error.WriteLine ( "({0},{1}) Syntax error - unknown char \"{2}\"", token.Line, token.Column, token.Text );
+
+            Console.ForegroundColor = colr;
+
+            return true;
+        }
+
+        // -----------------------------------------------
+
+        public bool Parse()
+        {
+            this.InitLexer (  );
+
+            foreach (SyntaxToken token in this.Tokenizer)
             {
-                if ( lexerToken.CheckChar ( this.current, false ) != TokenStatus.Accept ) continue;
+                if (this.PrintSyntaxError(token)) continue;
 
-                return this.ExecuteLexerToken ( lexerToken );
+                Console.Write ( token.Kind.ToString() + " : " );
+                Console.WriteLine ( token.Value );
             }
 
-            //SyntaxToken UnknownToken = new SyntaxToken ( SyntaxKind.Unknown, this.position, this.current.ToString(), null );
-
-            this.position++;
-
-            return null;
-        }
-
-        // -----------------------------------------------
-
-        private bool NextChar (  )
-        {
-            this.position++;
-
-            return this.position <= this.Text.Length;
-        }
-
-        // -----------------------------------------------
-
-        private bool IsEnde (  )
-        {
-            return this.position >= this.Text.Length;
-        }
-
-        // -----------------------------------------------
-
-        private SyntaxToken ExecuteLexerToken ( ILexerToken lexer )
-        {
-            int start = this.position;
-
-            while ( lexer.CheckChar ( this.current, false ) == TokenStatus.Accept )
-            {
-                if (!this.NextChar (  )) return null;
-            }
-
-            string text = this.Text.Substring ( start, this.position - start );
-
-            //return new SyntaxToken ( lexer.Kind, this.position, text, lexer.GetValue ( text ) );
-
-            return null;
-        }
-
-        // -----------------------------------------------
-
-        public bool MoveNext()
-        {
-            this.Current = this.NextToken (  );
-            return this.Current != null;
-        }
-
-        // -----------------------------------------------
-
-        public void Reset()
-        {
-            this.position = 0;
-        }
-
-        // -----------------------------------------------
-
-        public IEnumerator GetEnumerator()
-        {
-            return this;
+            return true;
         }
 
         // -----------------------------------------------
@@ -159,6 +151,7 @@ namespace LearnCsStuf.Basic
         #endregion methods
 
         // -----------------------------------------------
+
     }
 }
 
