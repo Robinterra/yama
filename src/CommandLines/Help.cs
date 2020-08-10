23fd06ea243420e9fd33336f3fbe95da87c2eae6
Robin D'Andrea <robin.dandrea@versalitic.com>
2020-08-10 08:20:38 +0200

    small fix - in help example arguments

--- src/CommandLines/Help.cs
+++ src/CommandLines/Help.cs
@@ -46,7 +46,7 @@ namespace LearnCsStuf.CommandLines
         public bool Print (  )
         {
             Console.WriteLine ( "Compiler for Yama, a Object-oriented Language for Microcontroller like ARM Cortex-M and AVR" );
-            Console.WriteLine ( "yama define atmega328p out out.S def avr inc bin/Debug/netcoreapp3.1/atmega328p bin/Debug/netcoreapp3.1/iftest.y" );
+            Console.WriteLine ( "yama out out.S define atmega328p def avr inc bin/Debug/netcoreapp3.1/System bin/Debug/netcoreapp3.1/iftest.yama" );
             Console.WriteLine (  );
 
             foreach ( ICommandLine line in this.CommandLines )
