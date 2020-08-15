cbb333c62fb74ef0fb9df696b03418e2ede3fb4a
Robin D'Andrea <robin.dandrea@versalitic.com>
2020-08-15 18:21:47 +0200

    now hex number in yama aviable

--- src/Yama/Lexer/Tokens/Digit.cs
+++ src/Yama/Lexer/Tokens/Digit.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Yama.Lexer
 {
     public class Digit : ILexerToken
@@ -33,12 +35,33 @@ namespace Yama.Lexer
 
             if ( !char.IsDigit ( lexer.CurrentChar ) ) return TokenStatus.Cancel;
 
-            while ( char.IsDigit ( lexer.CurrentChar ) )
+            bool isFirst0 = lexer.CurrentChar == '0';
+            bool ishex = false;
+
+            while ( this.CheckCurrentChar ( lexer.CurrentChar, ishex ) )
             {
+                lexer.NextChar (  );
+
+                if (!isFirst0) continue;
+
+                isFirst0 = false;
+
+                bool isok = lexer.CurrentChar == 'x';
+                ishex = isok;
+                if (!isok) isok = lexer.CurrentChar == 'b';
+                if (!isok) return TokenStatus.Complete;
+
                 lexer.NextChar (  );
             }
 
             return TokenStatus.Complete;
+        }
+
+        private bool CheckCurrentChar(char currentChar, bool ishex)
+        {
+            if (!ishex) return char.IsDigit ( currentChar );
+
+            return currentChar.IsHex();
         }
 
         // -----------------------------------------------
@@ -46,6 +69,12 @@ namespace Yama.Lexer
         public object GetValue ( byte[] daten )
         {
             string text = System.Text.Encoding.UTF8.GetString ( daten );
+
+            try
+            {
+                if (text.Contains("0x")) return int.Parse(text.Replace("0x", string.Empty), System.Globalization.NumberStyles.HexNumber);
+            }
+            catch { }
 
             if (!int.TryParse ( text, out int result )) return 0;
 
@@ -59,6 +88,19 @@ namespace Yama.Lexer
         // -----------------------------------------------
 
     }
+}
+
+/**
+ * @link https://stackoverflow.com/questions/228523/char-ishex-in-c-sharp
+ */
+public static class Extensions
+{
+   public static bool IsHex(this char c)
+   {
+      return   (c >= '0' && c <= '9') ||
+               (c >= 'a' && c <= 'f') ||
+               (c >= 'A' && c <= 'F');
+   }
 }
 
 // -- [EOF] --
